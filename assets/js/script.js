d6a78106da9ef6dd5bc950da70f9852b2aa50ffc
Diovani <diovanips@hotmail.com>
2021-06-29 10:05:07 -0300

    'Angular-Clone'

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function button_hamb(){
+    if(document.getElementById('sidebar').style.display == "none"){
+        document.getElementById('sidebar').style.display = "block";
+    }else{
+        document.getElementById('sidebar').style.display = "none";
+    }
+    
+}
+
+
+
+
+
